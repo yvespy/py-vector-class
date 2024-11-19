116f5f4ff30364a8e0f082d01dfd1b06a8703b33
yevpy <eugene2piskun@gmail.com>
2024-11-20 01:24:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
+import math
+from typing import Tuple
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_value: float, y_value: float) -> None:
+        self.x = round(x_value, 2)
+        self.y = round(y_value, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector") -> float:
+        if isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: Tuple,
+                                    end_point: Tuple) -> "Vector":
+        x_value = end_point[0] - start_point[0]
+        y_value = end_point[1] - start_point[1]
+        return cls(x_value, y_value)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+
+        if length != 0:
+            return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        dot_product = self * other
+        lengths_product = self.get_length() * other.get_length()
+        cos_a = max(min(dot_product / lengths_product, 1), -1)
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> float:
+        reference_vector = Vector(0, 1)
+        return self.angle_between(reference_vector)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        cos_theta = math.cos(radians)
+        sin_theta = math.sin(radians)
+        new_x = self.x * cos_theta - self.y * sin_theta
+        new_y = self.x * sin_theta + self.y * cos_theta
+        return Vector(new_x, new_y)
